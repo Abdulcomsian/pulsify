5ce87ed3c6e9893c28cbd2d84df30db8e6eb835b
Hamza <Hamza.Alam@accrualgroup.com>
2022-05-26 14:27:49 +0500

    HomePage With Responsive

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -293,6 +293,205 @@
                 </div>
             </div>
         </div>
+        <div class="patientFeedBack">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h3 class="commonTitle">Our Patients Feedback About Us</h3>
+                        <p>their impression after using our service</p>
+                    </div>
+                </div>
+                <div class="patientsSlider">
+                    <div class="patientsBox">
+                        <div class="quoteImg">
+                            <img src="{{asset('img/quote-right.png')}}" alt="" class="img-fluid">
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  tincidunt et scelerisque etiam. Blandit hendrerit id nec elementum ligula et.</p>
+                        <div class="profileDiv">
+                            <img src="{{asset('img/doctor1.png')}}" alt="" class="img-fluid">
+                            <div class="profileDetail">
+                                <h4>David Mark</h4>
+                                <span>Patient</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="patientsBox">
+                        <div class="quoteImg">
+                            <img src="{{asset('img/quote-right.png')}}" alt="" class="img-fluid">
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  tincidunt et scelerisque etiam. Blandit hendrerit id nec elementum ligula et.</p>
+                        <div class="profileDiv">
+                            <img src="{{asset('img/doctor1.png')}}" alt="" class="img-fluid">
+                            <div class="profileDetail">
+                                <h4>David Mark</h4>
+                                <span>Patient</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="patientsBox">
+                        <div class="quoteImg">
+                            <img src="{{asset('img/quote-right.png')}}" alt="" class="img-fluid">
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  tincidunt et scelerisque etiam. Blandit hendrerit id nec elementum ligula et.</p>
+                        <div class="profileDiv">
+                            <img src="{{asset('img/doctor1.png')}}" alt="" class="img-fluid">
+                            <div class="profileDetail">
+                                <h4>David Mark</h4>
+                                <span>Patient</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="patientsBox">
+                        <div class="quoteImg">
+                            <img src="{{asset('img/quote-right.png')}}" alt="" class="img-fluid">
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  tincidunt et scelerisque etiam. Blandit hendrerit id nec elementum ligula et.</p>
+                        <div class="profileDiv">
+                            <img src="{{asset('img/doctor1.png')}}" alt="" class="img-fluid">
+                            <div class="profileDetail">
+                                <h4>David Mark</h4>
+                                <span>Patient</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="leatestNew">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h3 class="commonTitle">Latest News</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu tincidunt et scelerisque<br> etiam. Blandit hendrerit id nec elementum ligula et.</p>
+                    </div>
+                </div>
+                <div class="multiNews">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="newsDiv">
+                                <div class="imgDiv">
+                                    <img src="{{asset('img/news1.png')}}" alt="" class="img-fluid">
+                                </div>
+                                <div class="newsContent">
+                                    <span><i class="fa fa-clock-o" aria-hidden="true"></i> January 1, 2022</span>
+                                    <h5>High Quality Treatment For Kids.</h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam</p>
+                                    <a href="">Read more <span><i class="fa fa-angle-right"></i></span></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="newsDiv">
+                                <div class="imgDiv">
+                                    <img src="{{asset('img/news2.png')}}" alt="" class="img-fluid">
+                                </div>
+                                <div class="newsContent">
+                                    <span><i class="fa fa-clock-o" aria-hidden="true"></i> January 1, 2022</span>
+                                    <h5>High Quality Treatment For Kids.</h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam</p>
+                                    <a href="">Read more <span><i class="fa fa-angle-right"></i></span></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="newsDiv">
+                                <div class="imgDiv">
+                                    <img src="{{asset('img/news3.png')}}" alt="" class="img-fluid">
+                                </div>
+                                <div class="newsContent">
+                                    <span><i class="fa fa-clock-o" aria-hidden="true"></i> January 1, 2022</span>
+                                    <h5>High Quality Treatment For Kids.</h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam</p>
+                                    <a href="">Read more <span><i class="fa fa-angle-right"></i></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="footer-widgets">
+                        <img src="{{asset('img/footerLogo.png')}}" alt="" class="img-fluid">
+                        <p>Address Here</p>
+                        <div class="socialDiv">
+                            <ul>
+                            <li>
+                                    <a href="">
+                                        <img src="{{asset('img/youtube.png')}}" alt="" class="img-fluid">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <img src="{{asset('img/facebook.png')}}" alt="" class="img-fluid">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <img src="{{asset('img/whatsapp.png')}}" alt="" class="img-fluid">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="footer-widgets">
+                        <h4>Company Info</h4>
+                        <ul>
+                            <li>
+                                <a href="">About Us</a>
+                            </li>
+                            <li>
+                                <a href="">Career</a>
+                            </li>
+                            <li>
+                                <a href="">Latest News</a>
+                            </li>
+                            <li>
+                                <a href="">Service Info</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="footer-widgets">
+                        <h4>Use Links</h4>
+                        <ul>
+                            <li>
+                                <a href="">Terms and Conditions</a>
+                            </li>
+                            <li>
+                                <a href="">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="">Contact us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="footer-widgets">
+                        <h4>Contact Info.</h4>
+                        <ul>
+                            <li>
+                                <a href="">+626564465455</a>
+                            </li>
+                            <li>
+                                <a href="">info@company.com</a>
+                            </li>
+                            <li>
+                                <a href="">Tel: +5646544654</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
     @endsection
 
